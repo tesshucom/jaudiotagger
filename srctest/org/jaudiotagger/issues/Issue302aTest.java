dcde94a06f612f8aa0189e0652b00310bad901be
ijabz <ijabz@fastmail.fm>
2020-03-26 11:22:20 +0000

    #302:java.nio.BufferUnderflowException when reading a corrupted mp3 file

---
+++ srctest/org/jaudiotagger/issues/Issue302aTest.java
@@ -0,0 +1,47 @@
+package org.jaudiotagger.issues;
+
+import org.jaudiotagger.AbstractTestCase;
+import org.jaudiotagger.audio.AudioFile;
+import org.jaudiotagger.audio.AudioFileIO;
+import org.jaudiotagger.tag.FieldKey;
+import org.jaudiotagger.tag.id3.ID3v22Tag;
+import org.jaudiotagger.tag.id3.ID3v24Tag;
+
+import java.io.File;
+
+import static org.jaudiotagger.tag.FieldKey.ALBUM;
+
+public class Issue302aTest extends AbstractTestCase
+{
+    /**
+     * Test Read Mp3
+     */
+    public void testReadMp3()
+    {
+        ID3v24Tag v24tag = null;
+        ID3v22Tag v22tag = null;
+        File orig = new File("testdata", "test302.mp3");
+        if (!orig.isFile())
+        {
+            return;
+        }
+
+        Exception exceptionCaught = null;
+        try
+        {
+            File testFile = AbstractTestCase.copyAudioToTmp("test302.mp3");
+            AudioFile af = AudioFileIO.read(testFile);
+            af.getTag().setField(ALBUM,"012345678901");
+            af.commit();
+
+            af = AudioFileIO.read(testFile);
+            assertEquals(af.getTag().getFirst(FieldKey.ALBUM), "012345678901");
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            exceptionCaught = e;
+        }
+        assertNull(exceptionCaught);
+    }
+}
